6a05b8897a799eab8eb428d4e48c7038f2aa72ba
Wertzui123 <46199283+Wertzui123@users.noreply.github.com>
2019-10-13 13:40:42 +0200

    Untested update for BedrockClans 2.0

--- BedrockClansAddon.php
+++ BedrockClansAddon.php
@@ -1,70 +1,87 @@
 <?php
-declare(strict_types = 1);
+declare(strict_types=1);
 /**
  * @name BedrockClansAddon
  * @version 1.0.0
  * @main   JackMD\ScoreHud\Addons\BedrockClansAddon
  * @depend BedrockClans
  */
-namespace JackMD\ScoreHud\Addons
-{
+
+namespace JackMD\ScoreHud\Addons {
+
     use JackMD\ScoreHud\addon\AddonBase;
     use pocketmine\Player;
     use Wertzui123\BedrockClans\Main;
-    class BedrockClansAddon extends AddonBase{
+
+    class BedrockClansAddon extends AddonBase
+    {
         /** @var Main */
         private $BedrockClans;
-        public function onEnable(): void{
+
+        public function onEnable(): void
+        {
             $this->BedrockClans = $this->getServer()->getPluginManager()->getPlugin("BedrockClans");
         }
+
         /**
          * @param Player $player
          * @return array
          */
-        public function getProcessedTags(Player $player): array{
+        public function getProcessedTags(Player $player): array
+        {
             return [
-                "{clan_name}"   => $this->getPlayerClan($player),
-		"{clan_rank}"   => $this->getPlayerClanRank($player),
-	        "{clan_members}"   => $this->getPlayerClanMembers($player)
+                "{clan_name}" => $this->getPlayerClan($player),
+                "{clan_rank}" => $this->getPlayerClanRank($player),
+                "{clan_members}" => $this->getPlayerClanMembers($player)
             ];
         }
+
         /**
          * @param Player $player
+         * @return string
          */
-        public function getPlayerClan(Player $player): string{
-			if($this->BedrockClans->isInClan($player)){
-		$clan = $this->BedrockClans->getClan($player);
-                return $clan->get("name");
-            }else{
+        public function getPlayerClan(Player $player): string
+        {
+            $player = $this->BedrockClans->getPlayer($player);
+			if ($player->isInClan()) {
+                $clan = $player->getClan();
+                return $clan->getName();
+            } else {
                 return "No Clan";
             }
 		}
-	    
-	            /**
+
+        /**
          * @param Player $player
+         * @return string
          */
-        public function getPlayerClanRank(Player $player){
-			if($this->BedrockClans->isInClan($player)){
-				if($this->BedrockClans->isLeader($player)){
-                return "Leader";
-				}else{
-					return "Member";
-				}
-            }else{
+        public function getPlayerClanRank(Player $player)
+        {
+            $player = $this->BedrockClans->getPlayer($player);
+            if ($player->isInClan()) {
+                if ($player->isLeader()) {
+                    return "Leader";
+                } else {
+                    return "Member";
+                }
+            } else {
                 return "No Clan";
             }
-		}
-	    
-	            /**
+        }
+
+        /**
          * @param Player $player
+         * @return string
          */
-        public function getPlayerClanMembers(Player $player){
-			if($this->BedrockClans->isInClan($player)){
-		$clan = $this->BedrockClans->getClan($player);
-                return implode(" ", $clan->get("members"));
-            }else{
+        public function getPlayerClanMembers(Player $player)
+        {
+            $player = $this->BedrockClans->getPlayer($player);
+            if ($player->isInClan()) {
+                $clan = $player->getClan();
+                return implode(", ", $clan->getMembers());
+            } else {
                 return "No Clan";
             }
-		}
+        }
     }
 }
